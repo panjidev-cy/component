17d313f884d11926b38857d12ff77afade267053
panjidev-cy <panjibhekti1@gmail.com>
2024-09-03 08:53:15 +0700

    header 0.1

--- src/components/Page/Home.jsx
+++ src/components/Page/Home.jsx
@@ -4,14 +4,30 @@
 function Home() {
     return(
         <>
-            <header>
-                <div className="logo">
-                    <div className="icon">
-                        <i className="uil uil-backpack" />
+            <header className="flex py-4 px-7 w-full gap-4 items-center justify-between fixed top-0 left-0 right-0 h-16 bg-dark">
+                <div className="flex items-center gap-2">
+                    <div className="w-8 h-8 flex items-center justify-center p-2 bg-primer rounded-lg">
+                        <i className="uil uil-backpack text-xl text-secondary" />
                     </div>
-                    <div className="name">
-                        <h2>Backpack</h2>
+                    <div className="">
+                        <h2 className="text-lg font-semibold capitalize font-poppins text-gray-100">Back<span className="text-primer italic font-semibold">pack</span></h2>
                     </div>
+                </div>
+                <form action="/search" method="get" className="flex gap-2">
+                    <input 
+                    type="text" 
+                    name="query" 
+                    placeholder="Search..." 
+                    required  
+                    className="px-4 py-1 h-8 border-[1.5px] border-primer rounded-md focus:outline-none focus:ring-0  "/>
+                    <button 
+                    type="submit" className="w-8 h-8 flex items-center justify-center p-2 bg-primer rounded-lg">
+                        <i className="uil uil-search-alt text-xl text-secondary"></i>
+                    </button>
+                </form>
+
+                <div className="w-8 h-8 flex items-center justify-center p-2 bg-primer rounded-lg">
+                    <i className="uil uil-github-alt text-xl text-secondary"></i>
                 </div>
             </header>
         </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'manrope': ['Manrope', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'dark': '#0e0741',
+        'light': '#ffffff',
+        'primer' : '#8236fd',
+        'secundary': '#ababb9',
+      },
+    },
   },
   plugins: [],
 }
