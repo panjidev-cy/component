30dac7119cd2eaa9969e510ff9e7945b081e8cc7
panjidev-cy <panjibhekti1@gmail.com>
2024-09-03 09:16:25 +0700

    pemecahan komponen

---
+++ src/components/Element/IconBadge.jsx
@@ -0,0 +1,11 @@
+
+
+function IconBadge(){
+    return(
+        <div className="w-8 h-8 flex items-center justify-center p-2 bg-primer rounded-lg">
+            <i className="uil uil-backpack text-xl text-secondary" />
+        </div>
+    )
+}
+
+export default IconBadge;
