17efd232537794a0f7beb5b97f3899b0b4298a7a
panjidev-cy <panjibhekti1@gmail.com>
2024-09-03 07:47:11 +0700

    start

---
+++ src/components/Page/Home.jsx
@@ -0,0 +1,21 @@
+
+
+
+function Home() {
+    return(
+        <>
+            <header>
+                <div className="logo">
+                    <div className="icon">
+                        <i className="uil uil-backpack" />
+                    </div>
+                    <div className="name">
+                        <h2>Backpack</h2>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Home;
