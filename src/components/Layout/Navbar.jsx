34adb14f18ec20ae9af1675b1e893a563c37c79e
panjidev-cy <panjibhekti1@gmail.com>
2024-09-04 21:01:35 +0700

    header responsive

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
             className="flex items-center gap-2 relative right-10 opacity-0"
             id="icon">
                 <IconBadge icon="uil-backpack"/>
-                <div className="">
+                <div className="md:hidden">
                     <h2 className="text-lg font-semibold capitalize font-poppins text-gray-100">
                         Back<span className="text-primer italic font-semibold">pack</span>
                     </h2>
@@ -37,7 +37,7 @@ function Navbar() {
                     className="px-4 py-1 h-8 border-[1.5px] border-primer rounded-md focus:outline-none focus:ring-0  bg-primer/30 text-gray-200 font-poppins text-xs"/>
                     <button 
                     type="submit" 
-                    className="w-8 h-8 flex items-center justify-center p-2 bg-primer rounded-lg">
+                    className="w-8 h-8 flex items-center justify-center p-2 bg-primer rounded-lg md:hidden">
                         <i className="uil uil-search-alt text-xl text-secondary"></i>
                     </button>
                 </form>
